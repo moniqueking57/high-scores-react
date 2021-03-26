6181350d3ac220a0a244eda55c1df39b9c6383bb
Monique <moniqueking57@gmail.com>
2021-03-26 12:08:25 +0000

    finished css needs work

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Country from "./component/Country";
 import Header from "./component/Header";
 import Scores from "./component/Scores";
 import allCountryScores from "./data/allCountryScores";
+import React, { useState } from "react";
+import AllScores from "./component/AllScores";
 
 function App() {
   const sortCountries = allCountryScores.sort((a, b) => {
@@ -13,33 +15,69 @@ function App() {
     }
   });
 
-  // button functionality
-  // function Sort(){
-  //   const
-  // }
+  let allScores = [];
+  for (let i = 0; i < allCountryScores.length; i++) {
+    allScores = allScores.concat(allCountryScores[i].scores).sort((a, b) => {
+      if (a.s < b.s) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+  }
+  const [sort, setSort] = useState("descending");
+  const clickHandler = () => {
+    setSort((sort) => {
+      if (sort === "descending") {
+        return "ascending";
+      } else {
+        return "descending";
+      }
+    });
+  };
 
   return (
     <div className="App">
-      <Header />
-      <button>Sort results</button>
+      <Header title={"Scores on the Doors"} />
+      <table className="table">
+        {allScores.map((country, index) => (
+          <AllScores n={country.n} s={country.s} key={index} />
+        ))}
+      </table>
+      <h4>Top Country Scores</h4>
+      <button onClick={clickHandler}>Click to sort scores</button>
       {sortCountries.map((country, index) => (
         <div key={index}>
           <Country name={country.name} />
-          {country.scores
-            .sort((a, b) => {
-              if (a.s < b.s) {
-                return 1;
-              } else {
-                return -1;
-              }
-            })
-            .map((score, index) => (
-              <Scores n={score.n} s={score.s} key={index} />
-            ))}
+          <Scores country={country} sortOrder={sort} />
         </div>
       ))}
     </div>
   );
 }
+
+//   return (
+//     <div className="App">
+//       <Header />
+//       <button>Sort results</button>
+//       {sortCountries.map((country, index) => (
+//         <div key={index}>
+//           <Country name={country.name} />
+//           {country.scores
+//             .sort((a, b) => {
+//               if (a.s < b.s) {
+//                 return 1;
+//               } else {
+//                 return -1;
+//               }
+//             })
+//             .map((score, index) => (
+//               <Scores n={score.n} s={score.s} key={index} />
+//             ))}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
 
 export default App;
